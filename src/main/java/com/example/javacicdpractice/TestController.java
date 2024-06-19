09b67385688490ddfb8c69445f58e3199375a7b7
WonyuChoi <phillip07290@naver.com>
2024-06-20 00:08:18 +0900

    feat: 개발 환경 테스트 컨트롤러 생성

---
+++ src/main/java/com/example/javacicdpractice/TestController.java
@@ -0,0 +1,20 @@
+package com.example.javacicdpractice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${test.environment.variable}")
+    private String testVariable;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/test")
+    public String test() {
+        return "common var: " + appName + "\ntest var: " + testVariable;
+    }
+}
