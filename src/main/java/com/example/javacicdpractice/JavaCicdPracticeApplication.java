6980ca6feb79507eb40cd7ab2e51cb4c0dc52711
WonyuChoi <phillip07290@naver.com>
2024-06-19 23:37:04 +0900

    setting: initial setting

---
+++ src/main/java/com/example/javacicdpractice/JavaCicdPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.javacicdpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCicdPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCicdPracticeApplication.class, args);
+    }
+}
